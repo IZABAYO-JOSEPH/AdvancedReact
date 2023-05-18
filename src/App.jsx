24961fb2e54a0d7e4eac859a7b98b423fdade1fa
Joseph Izabayo <joseph.izabayo@studentambassadors.com>
2023-05-18 17:56:27 +0200

    using usestate with ana rray of data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import Usestatehook from './usestatehook'
-
+import Usestatehook from './usestate/usestatehook'
+import UseStateArray from './usestate/useStateArray'
 const  App = () =>{
   return (
     <>
     <Usestatehook />
+    <UseStateArray />
     </>
      
      
---
+++ src/usestate/useStateArray.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { data } from "../data";
+const UseStateArray = () => {
+  const [ people, setPeaple]  = React.useState(data);
+  const removeItem = (id)=>{
+  const newpeople= people.filter((person)=>
+ person.id !==id)
+ setPeaple(newpeople)
+    }
+  const clearall = ()=>{
+    setPeaple([])
+  }
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            
+            <h4>{name}</h4>
+            <button type='button'onClick={()=>removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button type="button"style={{marginTop:'2rem'}}className="btn" onClick={()=>setPeaple([])}>clearAll</button>
+
+    </div>
+  );
+};
+
+export default UseStateArray;
