f01f42ed8150d1e88e542098d10fd72f57d779b8
Joseph Izabayo <joseph.izabayo@studentambassadors.com>
2023-06-06 02:44:58 +0200

    select input

--- src/reactforms/checkBox.jsx
+++ src/reactforms/checkBox.jsx
@@ -1,10 +1,15 @@
 import React,{useState} from 'react'
-
+const frameworks = ['react','nextjs','tailwind']
 const CheckBox = () => {
    const [shipping,setShipping] = useState(false)
+   const [framework,setFramework] = useState('react')
    const handleShipping = (e)=>{
     console.log(e.target.checked)
     setShipping(e.target.checked)
+   }
+
+   const handleFramework = (e) =>{
+    setFramework(e.target.value)
    }
   return (
     <form className='form'>
@@ -15,7 +20,11 @@ const CheckBox = () => {
        </div>
        <div className='form-row'style={{textAlign:'left'}}>
        <label htmlFor='framework' className='form-label'>Email</label>
-  <input type="checkbox"name="framework" id="framework" ></input>
+      <select name="framework" id= "id" value={framework} onChange={handleFramework}>
+        {frameworks.map((framework)=>{
+            return <option key={framework}>{framework}</option>
+        })}
+      </select>
        </div>
     <button type='submit' className='btn btn-block'>Submit</button>
     </form>
